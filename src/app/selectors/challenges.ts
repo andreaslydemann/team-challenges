99afd5830e23ab10e105fa4eb26dc265c0b2a589
Andreas Lüdemann <anld@danskebank.dk>
2018-09-24 17:06:16 +0200

    Use reselect in selectors

--- src/app/selectors/challenges.ts
+++ src/app/selectors/challenges.ts
@@ -1,96 +1,98 @@
+import { createSelector } from 'reselect';
 import { ChallengeModel, SubmissionModel } from '../models';
 import { RootState } from '../reducers';
 
-export function getChallengesTableData(state: RootState): ChallengeModel.ChallengesTableData[] {
-    const tableData: ChallengeModel.ChallengesTableData[] = [];
-
-    const { challenges } = state.challenges;
-    const { ratings } = state.ratings;
-    const { submissions } = state.submissions;
-
-    challenges.forEach(challenge => {
-        const i = submissions.findIndex(x => x.challengeId === challenge.id)
-
-        if (i !== -1) {
-            const j = ratings.findIndex(x => x.submissionId === submissions[i].id)
+export const getChallengesTableData = createSelector(
+    (state: RootState) => state.challenges.challenges,
+    (state: RootState) => state.submissions.submissions,
+    (state: RootState) => state.ratings.ratings,
+    (challenges, submissions, ratings) => {
+        const tableData: ChallengeModel.ChallengesTableData[] = [];
+        challenges.forEach(challenge => {
+            const i = submissions.findIndex(x => x.challengeId === challenge.id)
 
             if (i !== -1) {
-                tableData.push({
-                    key: challenge.id,
-                    name: challenge.name,
-                    status: submissions[i].status,
-                    timeline: submissions[i].createdAt,
-                    scorePercentage: ratings[j].scorePercentage,
-                    comment: ratings[j].comment,
-                });
+                const j = ratings.findIndex(x => x.submissionId === submissions[i].id)
+
+                if (i !== -1) {
+                    tableData.push({
+                        key: challenge.id,
+                        name: challenge.name,
+                        status: submissions[i].status,
+                        timeline: submissions[i].createdAt,
+                        scorePercentage: ratings[j].scorePercentage,
+                        comment: ratings[j].comment,
+                    });
+                } else {
+                    tableData.push({
+                        key: challenge.id,
+                        name: challenge.name,
+                        status: submissions[i].status,
+                        timeline: submissions[i].createdAt,
+                        scorePercentage: null,
+                        comment: "",
+                    });
+                }
             } else {
                 tableData.push({
                     key: challenge.id,
                     name: challenge.name,
-                    status: submissions[i].status,
-                    timeline: submissions[i].createdAt,
+                    status: SubmissionModel.Status.NOT_SUBMITTED,
+                    timeline: null,
                     scorePercentage: null,
                     comment: "",
                 });
             }
-        } else {
-            tableData.push({
-                key: challenge.id,
-                name: challenge.name,
-                status: SubmissionModel.Status.NOT_SUBMITTED,
-                timeline: null,
-                scorePercentage: null,
-                comment: "",
-            });
-        }
-    });
+        });
 
-    return tableData;
-}
+        return tableData;
+    }
+);
 
-export function getChallengeDetailsTableData(state: RootState): ChallengeModel.ChallengeDetailsTableData[] {
-    const tableData: ChallengeModel.ChallengeDetailsTableData[] = [];
+export const getChallengeDetailsTableData = createSelector(
+    (state: RootState) => state.submissions.submissions,
+    (state: RootState) => state.ratings.ratings,
+    (state: RootState) => state.teams.teams,
+    (submissions, ratings, teams) => {
+        const tableData: ChallengeModel.ChallengeDetailsTableData[] = [];
 
-    const { ratings } = state.ratings;
-    const { submissions } = state.submissions;
-    const { teams } = state.teams;
-
-    teams.forEach(team => {
-        const i = submissions.findIndex(x => x.teamId === team.id)
-
-        if (i !== -1) {
-            const j = ratings.findIndex(x => x.submissionId === submissions[i].id)
+        teams.forEach(team => {
+            const i = submissions.findIndex(x => x.teamId === team.id)
 
             if (i !== -1) {
-                tableData.push({
-                    key: team.id,
-                    team: team.name,
-                    status: submissions[i].status,
-                    timeline: submissions[i].createdAt,
-                    scorePercentage: ratings[j].scorePercentage,
-                    comment: ratings[j].comment,
-                });
+                const j = ratings.findIndex(x => x.submissionId === submissions[i].id)
+
+                if (i !== -1) {
+                    tableData.push({
+                        key: team.id,
+                        team: team.name,
+                        status: submissions[i].status,
+                        timeline: submissions[i].createdAt,
+                        scorePercentage: ratings[j].scorePercentage,
+                        comment: ratings[j].comment,
+                    });
+                } else {
+                    tableData.push({
+                        key: team.id,
+                        team: team.name,
+                        status: submissions[i].status,
+                        timeline: submissions[i].createdAt,
+                        scorePercentage: null,
+                        comment: "",
+                    });
+                }
             } else {
                 tableData.push({
                     key: team.id,
                     team: team.name,
-                    status: submissions[i].status,
-                    timeline: submissions[i].createdAt,
+                    status: SubmissionModel.Status.NOT_SUBMITTED,
+                    timeline: null,
                     scorePercentage: null,
                     comment: "",
                 });
             }
-        } else {
-            tableData.push({
-                key: team.id,
-                team: team.name,
-                status: SubmissionModel.Status.NOT_SUBMITTED,
-                timeline: null,
-                scorePercentage: null,
-                comment: "",
-            });
-        }
-    });
+        });
 
-    return tableData;
-}
+        return tableData;
+    }
+);
