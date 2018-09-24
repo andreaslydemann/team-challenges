fe30baac0aa034e904f6ca8fe275bc8840874374
Andreas Lüdemann <anld@danskebank.dk>
2018-09-24 22:50:26 +0200

    Rewrite challenge selectors using lodash

--- src/app/selectors/challenges.ts
+++ src/app/selectors/challenges.ts
@@ -1,98 +1,51 @@
 import { createSelector } from 'reselect';
-import { ChallengeModel, SubmissionModel } from '../models';
+import * as _ from 'lodash';
+import { SubmissionModel } from '../models';
 import { RootState } from '../reducers';
+import { omit } from '../utils';
 
 export const getChallengesTableData = createSelector(
     (state: RootState) => state.challenges.challenges,
     (state: RootState) => state.submissions.submissions,
     (state: RootState) => state.ratings.ratings,
     (challenges, submissions, ratings) => {
-        const tableData: ChallengeModel.ChallengesTableData[] = [];
-        challenges.forEach(challenge => {
+        const items = _.map(challenges, challenge => {
             const i = submissions.findIndex(x => x.challengeId === challenge.id)
 
-            if (i !== -1) {
-                const j = ratings.findIndex(x => x.submissionId === submissions[i].id)
-
-                if (i !== -1) {
-                    tableData.push({
-                        key: challenge.id,
-                        name: challenge.name,
-                        status: submissions[i].status,
-                        timeline: submissions[i].createdAt,
-                        scorePercentage: ratings[j].scorePercentage,
-                        comment: ratings[j].comment,
-                    });
-                } else {
-                    tableData.push({
-                        key: challenge.id,
-                        name: challenge.name,
-                        status: submissions[i].status,
-                        timeline: submissions[i].createdAt,
-                        scorePercentage: null,
-                        comment: "",
-                    });
-                }
-            } else {
-                tableData.push({
-                    key: challenge.id,
-                    name: challenge.name,
-                    status: SubmissionModel.Status.NOT_SUBMITTED,
-                    timeline: null,
-                    scorePercentage: null,
-                    comment: "",
-                });
-            }
+            return _.assign({ key: challenge.id, name: challenge.name },
+                i !== -1 ? { submissionId: submissions[i].id, status: submissions[i].status, timeline: submissions[i].createdAt } :
+                    { submissionId: null, status: SubmissionModel.Status.NOT_SUBMITTED, timeline: null });
         });
 
-        return tableData;
+        return _.map(items, item => {
+            const i = ratings.findIndex(x => x.submissionId === item.submissionId)
+
+            return omit(_.assign(item,
+                i !== -1 ? { scorePercentage: ratings[i].scorePercentage, comment: ratings[i].comment } :
+                    { scorePercentage: null, comment: "" }), 'submissionId');
+        });
     }
 );
 
 export const getChallengeDetailsTableData = createSelector(
+    (state: RootState) => state.teams.teams,
     (state: RootState) => state.submissions.submissions,
     (state: RootState) => state.ratings.ratings,
-    (state: RootState) => state.teams.teams,
-    (submissions, ratings, teams) => {
-        const tableData: ChallengeModel.ChallengeDetailsTableData[] = [];
-
-        teams.forEach(team => {
+    (teams, submissions, ratings) => {
+        const items = _.map(teams, team => {
             const i = submissions.findIndex(x => x.teamId === team.id)
 
-            if (i !== -1) {
-                const j = ratings.findIndex(x => x.submissionId === submissions[i].id)
-
-                if (i !== -1) {
-                    tableData.push({
-                        key: team.id,
-                        team: team.name,
-                        status: submissions[i].status,
-                        timeline: submissions[i].createdAt,
-                        scorePercentage: ratings[j].scorePercentage,
-                        comment: ratings[j].comment,
-                    });
-                } else {
-                    tableData.push({
-                        key: team.id,
-                        team: team.name,
-                        status: submissions[i].status,
-                        timeline: submissions[i].createdAt,
-                        scorePercentage: null,
-                        comment: "",
-                    });
-                }
-            } else {
-                tableData.push({
-                    key: team.id,
-                    team: team.name,
-                    status: SubmissionModel.Status.NOT_SUBMITTED,
-                    timeline: null,
-                    scorePercentage: null,
-                    comment: "",
-                });
-            }
+            return _.assign({ key: team.id, team: team.name },
+                i !== -1 ? { submissionId: submissions[i].id, status: submissions[i].status, timeline: submissions[i].createdAt } :
+                    { submissionId: null, status: SubmissionModel.Status.NOT_SUBMITTED, timeline: null });
         });
 
-        return tableData;
+        return _.map(items, item => {
+            const i = ratings.findIndex(x => x.submissionId === item.submissionId)
+
+            return omit(_.assign(item,
+                i !== -1 ? { scorePercentage: ratings[i].scorePercentage, comment: ratings[i].comment } :
+                    { scorePercentage: null, comment: "" }), 'submissionId');
+        });
     }
 );
